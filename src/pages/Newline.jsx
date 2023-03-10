34f635a3e7e8a652b21426a79298b8f222306fa4
Mohsen9april <Mohsen9april@gmail.com>
2023-03-11 00:07:32 +0330

    modify newline page

--- src/pages/Newline.jsx
+++ src/pages/Newline.jsx
@@ -1,40 +1,108 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const Newline = () => {
   return (
     <>
-      <div>
-        <section class="module parallax parallax-1">
-          <div class="NewLine_container">
-            <h1>Section 1</h1>
-          </div>
-        </section>
-
-        <section class="module parallax parallax-2">
-          <div class="NewLine_container">
-            <h1>Section 2</h1>
-          </div>
-        </section>
-
+      <Fragment>
         <section class="slide" id="slide2">
-          <h1>Set to full page for best experience.</h1>
+          <h1>عملیات خاک برداری</h1>
         </section>
+        <br />
+        <div>
+          <section class="module parallax parallax-1">
+            <div class="NewLine_container">
+              <h1>اردیبهشت ماه ۱۴۰۱</h1>
+            </div>
+          </section>
 
-        <section class="module parallax parallax-3">
-          <div class="NewLine_container">
-            <h1>Section 3</h1>
-          </div>
-        </section>
-        <section class="module parallax parallax-4">
-          <div class="NewLine_container">
-            <h1>Section 4</h1>
-          </div>
-        </section>
-      </div>
+          <section class="module parallax parallax-2">
+            <div class="NewLine_container">
+              <h1>اردیبهشت ماه ۱۴۰۱</h1>
+            </div>
+          </section>
+          <section class="module parallax parallax-3">
+            <div class="NewLine_container">
+              <h1>اردیبهشت ماه ۱۴۰۱</h1>
+            </div>
+          </section>
+          <section class="module parallax parallax-4">
+            <div class="NewLine_container">
+              <h1>اردیبهشت ماه ۱۴۰۱</h1>
+            </div>
+          </section>
+        </div>
+      </Fragment>
       <br />
+      <Fragment>
+        <section class="slide" id="slide2">
+          <h1>فاز دوم پروژه فنداسیون </h1>
+        </section>
+        <div>
+          <br />
+          <section class="module parallax parallax-5">
+            <div class="NewLine_container">
+              <h1>خرداد ماه ۱۴۰۱</h1>
+            </div>
+          </section>
+
+          <section class="module parallax parallax-6">
+            <div class="NewLine_container">
+              <h1>Section 2</h1>
+            </div>
+          </section>
+          <section class="module parallax parallax-7">
+            <div class="NewLine_container">
+              <h1>Section 3</h1>
+            </div>
+          </section>
+          <section class="module parallax parallax-8">
+            <div class="NewLine_container">
+              <h1>Section 4</h1>
+            </div>
+          </section>
+        </div>
+      </Fragment>
+      <br />
+
+      <Fragment>
+        <section class="slide" id="slide2">
+          <h1>فاز سوم پروژه ستون </h1>
+        </section>
+        <div>
+          <br />
+          <section class="module parallax parallax-9">
+            <div class="NewLine_container">
+              <h1>Section 1</h1>
+            </div>
+          </section>
+
+          <section class="module parallax parallax-10">
+            <div class="NewLine_container">
+              <h1>Section 2</h1>
+            </div>
+          </section>
+          <section class="module parallax parallax-11">
+            <div class="NewLine_container">
+              <h1>Section 3</h1>
+            </div>
+          </section>
+          <section class="module parallax parallax-12">
+            <div class="NewLine_container">
+              <h1>Section 4</h1>
+            </div>
+          </section>
+        </div>
+      </Fragment>
+      <br />
+
       <section class="slide" id="slide2">
-        <h1>Set to full page for best experience.</h1>
+        <h1>کاغذسازی مهرآیین پارس </h1>
       </section>
+
+      <div className="Newline_divider">
+        <img src="./images/Logo1.png" />
+      </div>
+
       <div>
         <div class="other-section"></div>
         <div class="cd"></div>
